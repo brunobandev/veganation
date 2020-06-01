a914865d5c65ae7364cfa0485a26f3c3a25fc3da
Bruno Bandeira <brunoban.dev@gmail.com>
2020-06-01 16:43:33 +0100

    separeted footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,19 +52,7 @@
             @yield('content')
         </main>
 
-        <footer>
-            <div class="container text-center">
-                <div class="d-flex justify-content-between">
-                    <div class=" text-left">
-                        2020 Copyright © Veganation. Todos os direitos reservados - Feito com <i class="fa fa-heart text-danger"></i> e respeito aos animais
-                    </div>
-                    <div class="text-right">
-                        <a href="mailto:veganation.app@gmail.com"><i class="fa fa-envelope"></i> veganation.app@gmail.com</a>
-                        <a class="ml-2" href="https://facebook.com/veganationapp" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        @include('layouts.footer')
     </div>
     @yield('scripts')
 </body>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container text-center">
+        <div class="d-flex justify-content-between">
+            <div class=" text-left">
+                2020 Copyright © Veganation. Todos os direitos reservados - Feito com <i class="fa fa-heart text-danger"></i> e respeito aos animais
+            </div>
+            <div class="text-right">
+                <a href="mailto:veganation.app@gmail.com"><i class="fa fa-envelope"></i> veganation.app@gmail.com</a>
+                <a class="ml-2" href="https://facebook.com/veganationapp" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a>
+            </div>
+        </div>
+    </div>
+</footer>
