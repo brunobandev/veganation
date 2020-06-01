31eb7959081403781d133c79ffc22374b264f372
Bruno Bandeira <brunoban.dev@gmail.com>
2020-06-01 11:02:39 +0100

    improviment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="keywords" content="Receitas, Vegano, Comunidade">
     <meta name="author" content="Bruno Bandeira & Luise Flores">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
