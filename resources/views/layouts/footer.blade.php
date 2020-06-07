63ddf0b0472ffa803c374d0107ea2d0468f6ff08
Bruno Bandeira <brunoban.dev@gmail.com>
2020-06-07 12:11:43 +0100

    footer responsive

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
-<footer>
+<footer class="">
     <div class="container text-center">
         <div class="d-flex justify-content-between">
-            <div class=" text-left">
+            <div class="text-left d-none d-lg-block">
                 2020 Copyright © Veganation. Todos os direitos reservados - Feito com <i class="fa fa-heart text-danger"></i> e respeito aos animais
             </div>
             <div class="text-right">
-                <a href="mailto:veganation.app@gmail.com"><i class="fa fa-envelope"></i> veganation.app@gmail.com</a>
-                <a class="ml-2" href="https://facebook.com/veganationapp" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a>
+                <a href="mailto:veganation.app@gmail.com"><i class="fa fa-envelope"></i></a>
+                <a class="ml-2" href="https://facebook.com/veganationapp" target="_blank"><i class="fab fa-facebook"></i> </a>
             </div>
         </div>
     </div>
